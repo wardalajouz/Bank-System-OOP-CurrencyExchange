3e66292ea9df5fa3d1f285c24b97ec1b09cb052a
Ward <wardziad7@gmail.com>
2025-04-11 19:21:14 +0300

    First upload of Bank System with OOP and Currency Exchange

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+
+{
+    while (true)
+    {
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+
+    }
+
+    return 0;
+}
+
